ba746225c8814b6e0a04c304c838aba43e43e9ac
Kevin Vazquez <kv937398@gmail.com>
2023-02-23 10:39:43 -0800

    The user file for the user profile section

---
+++ pages/user/[user].tsx
@@ -0,0 +1,9 @@
+import Layout from "@/components/layout";
+import React from "react";
+export default function User({ name }){
+    return (
+        <Layout title="">
+            
+        </Layout>
+    );
+}
